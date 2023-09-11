d52c5661975a171e1c5fbc87f0352ba357209b43
ixyhoo <ixyhoo@outlook.fr>
2023-09-11 21:42:09 +0200

    I create form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import { useRef } from "react";
+
 
 const App = () => {
+  const inputMessage = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    inputMessage.current.value = '';
+// logique d'envoie de données
+
+  };
+
   return (
     <div>
-      <h1>Hello React</h1>
+      <h2>Poster un message</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" placeholder='entrez un message' id="inputMessage" ref={inputMessage}/>
+        <input type="submit" />
+      </form>
+      <h2>Liste des messages</h2>
+      <div></div>
     </div>
   );
 };
